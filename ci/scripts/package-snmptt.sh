9c3724d933920b49f4fe88967aa2d40528073f5b
jfrileyiv <102501348+jfrileyiv@users.noreply.github.com>
2023-08-29 16:22:15 -0400

    Update package-snmptt.sh

--- ci/scripts/package-snmptt.sh
+++ ci/scripts/package-snmptt.sh
@@ -13,22 +13,22 @@ package_dir="${1}-${2}"
 
 rpmdev-setuptree
 
-cp ./ci/files/snmptt.service /root/rpmbuild/SOURCES/
+cp ./ci/files/snmptt.service ~/rpmbuild/SOURCES/
 
 sed -re "s/^(Version: ).+/\1${version}/" \
-  ./ci/files/snmptt.spec > /root/rpmbuild/SPECS/snmptt.spec
+  ./ci/files/snmptt.spec > ~/rpmbuild/SPECS/snmptt.spec
 
 ls -latr
 tar --create --gzip \
   --directory=. \
   --exclude=docs/build \
-  --file=/root/rpmbuild/SOURCES/snmptt-${version}.tgz \
+  --file=~/rpmbuild/SOURCES/snmptt-${version}.tgz \
   snmptt/
 
-rpmbuild -ba /root/rpmbuild/SPECS/snmptt.spec
+rpmbuild -ba ~/rpmbuild/SPECS/snmptt.spec
 
 mkdir package/
 mkdir "package/${package_dir}"
-find /root/rpmbuild/RPMS/ \
+find ~/rpmbuild/RPMS/ \
   -name '*.noarch.rpm' -exec cp {} "package/${package_dir}" \; , \
   -name '*.noarch.rpm' -exec rpm --query --info --package {} \;
