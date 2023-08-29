acadc4f6a5f8ac40c49fbb1462ea7d95370431b6
jfrileyiv <102501348+jfrileyiv@users.noreply.github.com>
2023-08-29 16:11:51 -0400

    Update package-snmptt.sh

--- ci/scripts/package-snmptt.sh
+++ ci/scripts/package-snmptt.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
+
+# ${1} = version tag (e.g. v1.5.1)
+# ${2} = build number (e.g. what build run from Jenkins this is)
+
 set -eux
 
 # ref=$(< .git/refs)
 # version=${ref#v} # v1.5.0 -> 1.5.0
 version=${1#v}
+build=${2}
+package_dir="${1}-${2}""
 
 rpmdev-setuptree
 
@@ -22,6 +28,7 @@ tar --create --gzip \
 rpmbuild -ba /root/rpmbuild/SPECS/snmptt.spec
 
 mkdir package/
+mkdir "package/${package_dir}"
 find /root/rpmbuild/RPMS/ \
-  -name '*.noarch.rpm' -exec cp {} "package/" \; , \
+  -name '*.noarch.rpm' -exec cp {} "package/${package_dir}" \; , \
   -name '*.noarch.rpm' -exec rpm --query --info --package {} \;
